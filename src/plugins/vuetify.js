364a8b665b701aaf290bc62156409976d34d1dd5
silverdev <ouaiji04@gmail.com>
2020-07-21 02:07:40 +0100

    Dashboard with vue

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors'
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    icons: {
+        iconfont: 'md' || 'fa'
+    },
+    theme: {
+        themes: {
+            light: {
+                backgound: colors.grey.lighten2,
+            },
+            dark: {
+                backgound: colors.shades.white
+            }
+        }
+    }
+});
